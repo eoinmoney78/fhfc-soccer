c29ee3e337772d8c2ca90cbb9f6fc27aa86e7801
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-05-24 07:11:56 -0400

    server-running

---
+++ app.js
@@ -0,0 +1,27 @@
+// Dependencies
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5002;
+
+const { db } = require('./db');
+
+// Middleware
+
+app.use(express.json());
+app.use(cors());
+
+
+
+const server = async () => {
+    db();
+    app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
+};
+
+server();
