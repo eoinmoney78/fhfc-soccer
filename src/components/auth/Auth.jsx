81fa131ed69f82801a2713a0f4e19c989a033aa3
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-10-19 20:05:32 -0400

    atlas update

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -25,12 +25,7 @@ function Auth(props) {
 
 };
 
-  // const handleLogout = () => {
-  //   setIsLoggedIn(false);
-  //   setIsAdmin(false);  // Set isAdmin state to false when user logs out
-  //   console.log('Admin status:', false);  // Log the admin status
-  //   props.updateToken('');
-  // };
+
 
   const handleLogout = () => {
     setIsLoggedIn(false);
@@ -81,76 +76,3 @@ function Auth(props) {
 
 export default Auth;
 
-
-
-// import React, { useState } from 'react';
-// import Signup from './signup/Signup';
-// import Login from './login/Login';
-
-// import { Button, Col, Container, Row } from 'reactstrap';
-// import Logout from '../auth/logout/Logout';
-// import { useTheme } from '@mui/material/styles';
-
-
-// function Auth(props) {
-//   const [button, setButton] = useState('To Signup');
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const theme = useTheme();
-
-//   const swapForm = () => {
-//     button === 'To Login' ? setButton('To Signup') : setButton('To Login');
-//   };
-
-//   const handleLogin = (token) => {
-//     setIsLoggedIn(true);
-//     props.updateToken(token);
-//   };
-
-//   const handleLogout = () => {
-//     setIsLoggedIn(false);
-//     props.updateToken('');
-//   };
-
-//   const displayForm = () => {
-//     return button === 'To Login' ? (
-//       <Container>
-//         <Row>
-//           <Col md="4">
-//             <Signup updateToken={props.updateToken} />
-//           </Col>
-//         </Row>
-//       </Container>
-//     ) : (
-//       <Container>
-//         <Row>
-//           <Col md="4">
-//             <Login updateToken={handleLogin} />
-//           </Col>
-//         </Row>
-//       </Container>
-//     );
-//   };
-
-//   return (
-//     <>
-//       {isLoggedIn ? (
-//         <Logout onLogout={handleLogout} />
-//       ) : (
-//         <Button
-//         onClick={swapForm}
-//         style={{
-//           margin: '.5em',
-//           color: theme.palette.secondary.main,
-//         }}
-//       >
-//         {button}
-//       </Button>
-      
-//       )}
-//       <br />
-//       {displayForm()}
-//     </>
-//   );
-// }
-
-// export default Auth;
